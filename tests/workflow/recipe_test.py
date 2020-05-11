2bafe7c9a81bfc0a9c960f0e8f23dea5adfa01f7
AntoineDao <antoinedao1@gmail.com>
2020-05-11 13:45:59 +0100

    test(recipe): add recipe tests

---
+++ tests/workflow/recipe_test.py
@@ -0,0 +1,26 @@
+import yaml
+from tests.base.io_test import BaseIOTest
+from tests.base.value_error import BaseValueErrorTest
+from tests.base.folder_test import BaseFolderTest
+
+from queenbee.recipe import Recipe
+
+ASSET_FOLDER = 'tests/assets/recipes'
+
+class TestIO(BaseIOTest):
+
+    klass = Recipe
+
+    asset_folder = ASSET_FOLDER
+
+class TestValueError(BaseValueErrorTest):
+
+    klass = Recipe
+
+    asset_folder = ASSET_FOLDER
+
+class TestFolder(BaseFolderTest):
+
+    klass = Recipe
+
+    asset_folder = ASSET_FOLDER
