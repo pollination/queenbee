5aab0774c3b6da3e66de7764ee346433beb9f9a8
Mostapha <sadeghipour@gmail.com>
2020-10-25 10:25:19 -0400

    feat(openapi): add docs.py

---
+++ docs.py
@@ -0,0 +1,89 @@
+import json
+import argparse
+
+from pkg_resources import get_distribution
+from pydantic_openapi_helper.core import get_openapi
+from pydantic_openapi_helper.inheritance import class_mapper
+from queenbee.recipe.recipe import Recipe
+from queenbee.operator.operator import Operator
+from queenbee.io.dag import DAGBooleanInput
+
+
+parser = argparse.ArgumentParser(description='Generate OpenAPI JSON schemas')
+
+parser.add_argument('--version',
+                    help='Set the version of the new OpenAPI Schema')
+
+args = parser.parse_args()
+
+VERSION = None
+
+if args.version:
+    VERSION = args.version.replace('v', '')
+else:
+    VERSION = '.'.join(get_distribution('dragonfly_schema').version.split('.')[:3])
+
+
+info = {
+    "description": "",
+    "version": VERSION,
+    "title": "",
+    "contact": {
+        "name": "Ladybug Tools",
+        "email": "info@ladybug.tools",
+        "url": "https://github.com/ladybug-tools/queenbee"
+    },
+    "x-logo": {
+        "url": "https://www.ladybug.tools/assets/img/honeybee.png",
+        "altText": "Queenbee logo"
+    },
+    "license": {
+        "name": "MIT",
+        "url": "https://github.com/ladybug-tools/queenbee-schema/blob/master/LICENSE"
+    }
+}
+
+
+modules = [
+    {'module': Recipe, 'name': 'Recipe'}
+    # {'module': Operator, 'name': 'Operator'}
+]
+
+
+for module in modules:
+    # generate Recipe open api schema
+    print(f'Generating {module["name"]} documentation...')
+
+    external_docs = {
+        "description": "OpenAPI Specification with Inheritance",
+        "url": f"./{module['name'].lower()}_inheritance.json"
+    }
+
+    openapi = get_openapi(
+        [module['module']],
+        title=f'Queenbee {module["name"]} Schema',
+        description=f'Documentation for Queenbee {module["name"].lower()} schema.',
+        version=VERSION, info=info,
+        external_docs=external_docs)
+    # set the version default key in the Recipe schema
+    # openapi['components']['schemas']['Recipe']['properties']['version']['default'] = VERSION
+    with open(f'./docs/{module["name"].lower()}.json', 'w') as out_file:
+        json.dump(openapi, out_file, indent=2)
+
+    # with inheritance
+    openapi = get_openapi(
+        [module['module']],
+        title=f'Queenbee {module["name"]} Schema',
+        description=f'Documentation for Queenbee {module["name"].lower()} schema.',
+        version=VERSION, info=info,
+        inheritance=True,
+        external_docs=external_docs
+    )
+    # set the version default key in the Recipe schema
+    # openapi['components']['schemas']['Recipe']['allOf'][1]['properties']['version']['default'] = VERSION
+    with open(f'./docs/{module["name"].lower()}_inheritance.json', 'w') as out_file:
+        json.dump(openapi, out_file, indent=2)
+
+    # add the mapper file
+    with open(f'./docs/{module["name"].lower()}_mapper.json', 'w') as out_file:
+        json.dump(class_mapper([module['module']]), out_file, indent=2)
