ef0f6bfc99150b067e2f6e6e4393e59af7db0203
Antoine Dao <antoinedao1@gmail.com>
2021-03-10 15:15:24 +0000

    fix(status): enumerate job/run/step status strings
    This makes it clearer what status strings to expect and helps generate more robust SDK clients.

--- queenbee/job/run.py
+++ queenbee/job/run.py
@@ -14,6 +14,23 @@ from ..io.outputs.step import StepOutputs
 from .status import BaseStatus
 
 
+class StepStatusEnum(str, Enum):
+    """Enumaration of allowable status strings"""
+
+    # The step has been scheduled for execution
+    scheduled = 'Scheduled'
+    # The step is running
+    running = 'Running'
+    # The step has failed
+    failed = 'Failed'
+    # The step has finished succesfully
+    succeeded = 'Succeeded'
+    # The step has been skipped 
+    skipped = 'Skipped'
+    # Could not determine the status of the step
+    unknown = 'Unknown'
+
+
 class StatusType(str, Enum):
     """Type enum for status type."""
     Function = 'Function'
@@ -39,6 +56,11 @@ class StepStatus(BaseStatus):
         description='A human readable name for the step. Usually defined by the '
         'DAG task name but can be extended if the step is part of a loop for example. '
         'This name is unique within the boundary of the DAG/Job that generated it.'
+    )
+    
+    status: StepStatusEnum = Field(
+        StepStatusEnum.unknown,
+        description='The status of this step.'
     )
 
     status_type: StatusType = Field(
@@ -87,6 +109,25 @@ class StepStatus(BaseStatus):
     )
 
 
+class RunStatusEnum(str, Enum):
+    """Enumaration of allowable status strings"""
+    
+    # The run has been created
+    created = 'Created'
+    # The run has been scheduled for execution
+    scheduled = 'Scheduled'
+    # The run is being executed
+    running = 'Running'
+    # The run is being post-processed (status and folder cleanup)
+    post_processing = 'Post-Processing'
+    # The run has failed
+    failed = 'Failed'
+    # The run has completed succesfully
+    succeeded = 'Succeeded'
+    # Could not determine the status of the run
+    unknown = 'Unknown'
+
+
 class RunStatus(BaseStatus):
     """Job Status."""
     api_version: constr(regex='^v1beta1$') = Field('v1beta1', readOnly=True)
@@ -100,12 +141,17 @@ class RunStatus(BaseStatus):
 
     job_id: str = Field(
         ...,
-        description='The ID of the job that generated this run'
+        description='The ID of the job that generated this run.'
     )
 
     entrypoint: str = Field(
         None,
         description='The ID of the first step in the run.'
+    )
+
+    status: RunStatusEnum = Field(
+        RunStatusEnum.unknown,
+        description='The status of this run.'
     )
 
     steps: Dict[str, StepStatus] = {}
